84422802bb1ff8a1b70912cc06d2ad9bdca5753f
Bambang Wijonarko <45529720+B4mzMutt@users.noreply.github.com>
2018-12-07 00:40:09 +0700

    source code

---
+++ menampilkankata.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int tampil(int n) {
+	int i;
+	i <= n;
+	for (i = 1; i <= n; ++i)
+		cout << "ALGORITMA DAN PEMROGRAMAN" << endl;
+		return i;
+}
+int main() {
+	int i, n, hasil;
+	cout << "menampilkan ALGORITMA DAN PEMROGRAMAN" << endl;
+	cout << "jumlah kata yang ingin ditampilkan: ";
+	cin >> n;
+
+	hasil = tampil(n);
+	cout << hasil << endl;
+
+
+	cin.get();
+	return 0;
+
+}
